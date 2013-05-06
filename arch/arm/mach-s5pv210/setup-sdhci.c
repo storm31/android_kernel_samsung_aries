252470fba06d6a2e780709aab00edbfcec682e60
Humberto Borba <humberos@gmail.com>
2013-05-05 22:31:45 -0300

    source fixes (get compat for CM merges) - part 5
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- arch/arm/mach-s5pv210/setup-sdhci.c
+++ arch/arm/mach-s5pv210/setup-sdhci.c
@@ -88,18 +88,13 @@ void s5pv210_setup_sdhci_cfg_card(struct platform_device *dev,
 		if ((ios->clock > range_start) && (ios->clock < range_end))
 			ctrl3 = S3C_SDHCI_CTRL3_FCSELTX_BASIC |
 				S3C_SDHCI_CTRL3_FCSELRX_BASIC;
-		else if (machine_is_herring() && herring_is_cdma_wimax_dev() &&
-								dev->id == 2) {
+		else if (((machine_is_herring() && herring_is_cdma_wimax_dev()) ||
+					machine_is_p1()) && dev->id == 2) {
 			ctrl3 = S3C_SDHCI_CTRL3_FCSELTX_BASIC;
 			//if(card->type & MMC_TYPE_SDIO)
 				ctrl3 |= S3C_SDHCI_CTRL3_FCSELRX_BASIC;
 			//else
 			//	ctrl3 |= S3C_SDHCI_CTRL3_FCSELRX_INVERT;
-#ifdef CONFIG_MACH_P1
-		} else if (dev->id == 2) {
-			ctrl3 = S3C_SDHCI_CTRL3_FCSELTX_BASIC |
-				S3C_SDHCI_CTRL3_FCSELRX_BASIC;
-#endif
 		} else
 			ctrl3 = S3C_SDHCI_CTRL3_FCSELTX_BASIC |
 				S3C_SDHCI_CTRL3_FCSELRX_INVERT;
@@ -324,30 +319,20 @@ EXPORT_SYMBOL_GPL(sdhci_s3c_force_presence_change);
 void s3c_sdhci_set_platdata(void)
 {
 #if defined(CONFIG_S3C_DEV_HSMMC)
-	if (machine_is_herring() || machine_is_aries()) { /* TODO: move to mach-herring.c */
+	if (machine_is_herring() || machine_is_aries() || machine_is_p1()) {
+		/* TODO: move to mach-herring.c */
 		hsmmc0_platdata.cd_type = S3C_SDHCI_CD_PERMANENT;
 	}
-
-#ifdef CONFIG_MACH_P1
-	hsmmc0_platdata.cd_type = S3C_SDHCI_CD_PERMANENT;
-#endif
 
 	s3c_sdhci0_set_platdata(&hsmmc0_platdata);
 #endif
 #if defined(CONFIG_S3C_DEV_HSMMC1)
-	if (machine_is_aries()) {
+	if (machine_is_aries() || machine_is_p1()) {
 		hsmmc1_platdata.cd_type = S3C_SDHCI_CD_EXTERNAL;
 		hsmmc1_platdata.ext_cd_init = ext_cd_init_hsmmc1;
 		hsmmc1_platdata.ext_cd_cleanup = ext_cd_cleanup_hsmmc1;
 		hsmmc1_platdata.built_in = 1;
 	}
-
-#ifdef CONFIG_MACH_P1
-	hsmmc1_platdata.cd_type = S3C_SDHCI_CD_EXTERNAL;
-	hsmmc1_platdata.ext_cd_init = ext_cd_init_hsmmc1;
-	hsmmc1_platdata.ext_cd_cleanup = ext_cd_cleanup_hsmmc1;
-	hsmmc1_platdata.built_in = 1;
-#endif
 
 	s3c_sdhci1_set_platdata(&hsmmc1_platdata);
 #endif
@@ -368,36 +353,22 @@ void s3c_sdhci_set_platdata(void)
 		}
 	}
 
-	if (machine_is_aries()) {
+	if (machine_is_aries() || machine_is_p1()) {
 		hsmmc2_platdata.cd_type = S3C_SDHCI_CD_GPIO;
 		hsmmc2_platdata.ext_cd_gpio = S5PV210_GPH3(4);
 		hsmmc2_platdata.ext_cd_gpio_invert = true;
 		universal_sdhci2_cfg_ext_cd();
 	}
 
-#ifdef CONFIG_MACH_P1
-	hsmmc2_platdata.cd_type = S3C_SDHCI_CD_GPIO;
-	hsmmc2_platdata.ext_cd_gpio = S5PV210_GPH3(4);
-	hsmmc2_platdata.ext_cd_gpio_invert = true;
-	universal_sdhci2_cfg_ext_cd();
-#endif
-
 	s3c_sdhci2_set_platdata(&hsmmc2_platdata);
 #endif
 #if defined(CONFIG_S3C_DEV_HSMMC3)
-	if (machine_is_herring() || machine_is_aries()) {
+	if (machine_is_herring() || machine_is_aries() || machine_is_p1()) {
 		hsmmc3_platdata.cd_type = S3C_SDHCI_CD_EXTERNAL;
 		hsmmc3_platdata.ext_cd_init = ext_cd_init_hsmmc3;
 		hsmmc3_platdata.ext_cd_cleanup = ext_cd_cleanup_hsmmc3;
 		hsmmc3_platdata.built_in = 1;
 	}
-
-#ifdef CONFIG_MACH_P1
-	hsmmc3_platdata.cd_type = S3C_SDHCI_CD_EXTERNAL;
-	hsmmc3_platdata.ext_cd_init = ext_cd_init_hsmmc3;
-	hsmmc3_platdata.ext_cd_cleanup = ext_cd_cleanup_hsmmc3;
-	hsmmc3_platdata.built_in = 1;
-#endif
 
 	s3c_sdhci3_set_platdata(&hsmmc3_platdata);
 #endif
