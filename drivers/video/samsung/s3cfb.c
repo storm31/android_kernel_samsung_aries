74aad39fa91bc3fee8c2b1cccac7fd1cc377a3ad
Humberto Borba <humberos@gmail.com>
2013-05-10 15:00:27 -0300

    drivers: video: samsung: fix typo

--- drivers/video/samsung/s3cfb.c
+++ drivers/video/samsung/s3cfb.c
@@ -145,7 +145,7 @@ static int s3cfb_draw_logo(struct fb_info *fb)
 #endif
 	if (readl(S5P_INFORM5)) //LPM_CHARGING mode
 #ifdef CONFIG_FB_S3C_LVDS
-		printk(KERN_INFO "[LVDS] lmp charging mode\n");
+		printk(KERN_INFO "[LVDS] lpm charging mode\n");
 #else
 		memcpy(fb->screen_base, charging, fb->var.yres * fb->fix.line_length);
 #endif
