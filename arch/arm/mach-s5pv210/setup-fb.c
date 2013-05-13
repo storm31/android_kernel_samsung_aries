194bbaeebb3ab9b54157e74b48175d6fecb34838
Humberto Borba <humberos@gmail.com>
2013-05-13 02:23:22 -0300

    arm: s5pv210: increase mdnie clock from 24KHz to 30KHz
    fixed lcd lag when the tab wake.

--- arch/arm/mach-s5pv210/setup-fb.c
+++ arch/arm/mach-s5pv210/setup-fb.c
@@ -140,7 +140,7 @@ int s3cfb_clk_on(struct platform_device *pdev, struct clk **s3cfb_clk)
 			clk_set_parent(sclk_mdnie_pwm, clk_xusb);
 			clk_put(clk_xusb);
 			}
-		clk_set_rate(sclk_mdnie_pwm, 2400*1000);		// mdnie pwm need to 24Khz*100
+		clk_set_rate(sclk_mdnie_pwm, 3000*1000);		// mdnie pwm need to 30Khz*100
 		clk_put(sclk_mdnie_pwm);
 		}
 	}
