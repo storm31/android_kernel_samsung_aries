b93be51332ed5a6dc366c0dbf501d26df372f18a
storm31 <sergej.michel@t-online.de>
2013-09-21 14:27:59 +0200

    Revert "drivers: usb: aries|p1: add support for usb host"
    This reverts commit 45e2b6fd806dae5b78d7e751cef350d59cc3997a.

--- include/linux/sec_battery.h
+++ include/linux/sec_battery.h
@@ -34,8 +34,6 @@
 #define SAMSUNG_LPM_MODE
 //#define __FUEL_GAUGES_IC__
 
-#define BATTERY_CURRENT
-
 enum cable_type_t {
 	CABLE_TYPE_NONE = 0,
 	CABLE_TYPE_USB,
@@ -122,10 +120,6 @@ enum {
 	BATT_TEMP_CHECK,
 	BATT_FULL_CHECK,
 #endif
-#ifdef BATTERY_CURRENT
-	BATT_CURRENT,
-#endif
-	DISABLE_CHARGER,
 };
 
 
